a91c34822cca66d8a90561c77d76a4d5ae0fe1de
Vighnesh Kanakala <63389698+vighnesh777@users.noreply.github.com>
2021-10-29 10:37:34 +0530

    done

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post("/signin",function(req,res){
      client.close();  
      });  
      });
- res.sendFile(__dirname+"/Signin_success.html")  ;
+     res.redirect('http://localhost:3000/home');
     
 })
 
