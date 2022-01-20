2a778767246d9d95b9eae7460543886bcc683dc7
Vighnesh Kanakala <63389698+vighnesh777@users.noreply.github.com>
2022-01-20 18:53:08 +0530

    done

--- server.js
+++ server.js
@@ -1,47 +1,72 @@
 const express= require("express");
-const path = require('path');
-const bcrypt = require('bcryptjs');
+
 const bodyParser = require('body-parser');
-const https = require('https');
+
 var PORT=process.env.PORT || 3001;
-const options = {
-  
-  path: '/signin',
-  method: 'post'
-};
+
+
 
 const app= express();
-var MongoClient = require('mongodb').MongoClient;  
+// var MongoClient = require('mongodb').MongoClient;  
+const mongoose=require("mongoose");
+
 const { stringify } = require("querystring");
 var url = "mongodb+srv://test123:test123@cluster0.jxbu5.mongodb.net/Users?retryWrites=true&w=majority";  
 app.use(express.static('build'));
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+let cors = require('cors')
+app.use(cors())
+
+app.use(express.json());
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    gender:{type: String, required: true},
+    relation:{type: String, required: true},
+    dob:{type: String, required: true}
+  });
+const user=mongoose.model("User",userSchema)
+mongoose.connect(url,(err)=>{
+    if(err){console.log("Error Connecting to Data base");}
+    console.log("Connection OK");
+})
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 app.get("/",function(req,res){
     res.send("<h1>Server Up and Running</h1>");
 })
-app.post("/signin",function(req,res){
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+app.post("/add",function(req,res){
     
- MongoClient.connect(url,function (err, client){  
-
-     if (err) throw err;  
-     var db = client.db('Users');
-     var email1= req.body.Email;
-     var username= req.body.Username;
-     var password1=bcrypt.hashSync(req.body.password1, 10);
-     var myobj = { name: username, email: email1, password: password1 };  
-     console.log(email1,username,password1);
-     db.collection("User_details").insertOne(myobj, function(error, res) {  
-     if (error) throw error;  
-      
-
-     client.close();  
-     });  
-     });
-     res.redirect('http://localhost:3000/home');
+            var Name=req.body.Name;
+            var Gender=req.body.Gender;
+            var Relation=req.body.Relation;
+            var Dob=req.body.Dob;
+            const current=new user({name:Name,gender:Gender,relation:Relation,dob:Dob});
+            var connection=mongoose.connection;
+            console.log("Done");
+            connection.collection("users").insertOne(current,(err)=>{
+                if(err){res.sendStatus(403)}
+                console.log("Inserted");
+                res.sendStatus(200);
+            
+        })
     
 })
-
-
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+app.get("/getData",function(req,res){
+    
+            
+            // const current=new user({name:Name,gender:Gender,relation:Relation,dob:Dob});
+           
+            
+           user.find({},function(err,items){
+               res.json(items);
+           })
+            
+            console.log("Fetched");
+    
+})
 app.listen(PORT,function(){
     console.log("Server UP on "+PORT);
 })
